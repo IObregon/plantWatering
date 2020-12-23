267c65d2844680d55ce561622ef6ddff129c6e83
IObregon <ikerobregonreigosa@gmail.com>
2020-12-23 10:19:39 +0100

    If the plant need water, the relay is trigger.

--- wateringReader.go
+++ wateringReader.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"os"
 	"time"
+
 	"github.com/stianeikeland/go-rpio"
 )
 
 var (
-	pin = rpio.Pin(21)
+	pin       = rpio.Pin(21)
+	relay_pin = rpio.Pin(20)
 )
 
 func main() {
@@ -21,8 +23,13 @@ func main() {
 	pin.Input()
 
 	for true {
-		res := pin.Read()
-		fmt.Println(res)
+		readResult := pin.Read()
+		fmt.Println(readResult)
+		if readResult != 1 {
+			relay_pin.High()
+			time.Sleep(3 * time.Second)
+			relay_pin.Low()
+		}
 		time.Sleep(5 * time.Second)
 	}
 }
